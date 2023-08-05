198f8bd61f96a69f4eb007c0d1135c1d052540e5
Thulasi Ram <thulasi503@gmail.com>
2023-08-05 20:26:46 +0530

    feat: intial commit

---
+++ src/lib/Greet.svelte
@@ -0,0 +1,48 @@
+<script>
+  import { invoke } from '@tauri-apps/api/tauri'
+  import { Command } from '@tauri-apps/api/shell'
+  import { open } from '@tauri-apps/api/shell';
+
+
+
+  let name = ''
+  let greetMsg = ''
+
+  async function greet() {
+    console.log("call")
+    greetMsg = await invoke('greet', { name })
+  }
+
+  let devices = ''
+  let devicesErr = ''
+  async function adb_list_devices() {
+    await open('https://github.com/tauri-apps/tauri');
+
+    // const cmd = new Command('adb-devices');
+    // const cmdOutput = await cmd.execute();
+
+
+    // console.log(cmdOutput.code)
+    // console.log(cmdOutput.signal)
+    // console.log(cmdOutput.stderr)
+    // console.log(cmdOutput.stdout)
+    // const output = await new Command('echo', 'message').execute();
+    // console.log(output.code)
+    // console.log(output.signal)
+    // console.log(output.stderr)
+    // console.log(output.stdout)
+    devices = "test"
+    devicesErr = "test Err"
+  }
+</script>
+
+<div class="space-y-12">
+  <input id="greet-input" placeholder="Enter a name..." bind:value="{name}" />
+  <button on:click="{greet}">Greet</button>
+  <p>{greetMsg}</p>
+
+  <button on:click="{adb_list_devices}">ADB List</button>
+  <p>{devices}</p>
+  <p>{devicesErr}</p>
+
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Greet from '../lib/Greet.svelte'
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<Greet />
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
