722b08da8a0403c90d51648100bdb751eaffedd7
Thulasi Ram <thulasi503@gmail.com>
2023-08-21 15:43:59 +0530

    fix: logo positioning cosmetic fix

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -6,13 +6,12 @@
 
 <!-- https://flowbite-svelte.com/docs/components/navbar#Navbar -->
 <Navbar
-	let:hidden
-	let:toggle
     rounded color="form"
-	navDivClass="mx-0" 
+	navDivClass="flex justify-center mx-0" 
+	class="bg-transparent"
 >
 	<NavBrand href="/" >
-		<img src={SadLogo} alt="Simpl Android Debloater Logo" />
+		<img src={SadLogo} class="w-10 h-10" alt="Simpl Android Debloater Logo" />
 		<span class="self-center whitespace-nowrap text-l font-semibold dark:text-white pl-4">Simpl Android Debloater</span>
 	</NavBrand>
 </Navbar>
