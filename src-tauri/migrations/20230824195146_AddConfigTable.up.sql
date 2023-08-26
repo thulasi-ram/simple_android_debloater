bdd2ae6d9f01f50aa80e6be19ceb144bbbb52bca
Thulasi Ram <thulasi503@gmail.com>
2023-08-26 22:43:14 +0530

    feat: add support for db and config persistance

---
+++ src-tauri/migrations/20230824195146_AddConfigTable.up.sql
@@ -0,0 +1,5 @@
+-- Add up migration script here
+create table config (
+    id integer primary key,
+    data TEXT
+);
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,38 +10,57 @@ mod sad;
 mod store;
 mod users;
 
-use std::time::Duration;
+use std::{env, str::FromStr, time::Duration};
 
 use anyhow::anyhow;
 use err::ResultOkPrintErrExt;
 use events::{Event, PackageEvent};
-use log::error;
+use log::{error, info};
 use packages::Package;
 use serde::{Deserialize, Serialize};
+use sqlx::{sqlite::SqliteConnectOptions, Row, SqlitePool};
+
 use tauri::Manager;
 use tokio::{sync::mpsc, time};
 
 use devices::Device;
 use sad::SADError;
+use tauri_plugin_log::{fern::colors::ColoredLevelConfig, LogTarget};
 use users::User;
-use tauri_plugin_log::{LogTarget, fern::colors::ColoredLevelConfig};
 
-struct AsyncEventSender {
-    inner: tokio::sync::Mutex<mpsc::Sender<events::AsyncEvent>>,
+struct App {
+    pub db: tokio::sync::Mutex<SqlitePool>,
+    pub event_emitter: tokio::sync::Mutex<mpsc::Sender<events::AsyncEvent>>,
+    pub cache_store: tokio::sync::Mutex<store::Store>,
 }
 
-struct SadCache {
-    inner: tokio::sync::Mutex<store::Store>,
+impl App {
+    fn new(p: SqlitePool, s: mpsc::Sender<events::AsyncEvent>) -> Self {
+        Self {
+            db: tokio::sync::Mutex::new(p),
+            event_emitter: tokio::sync::Mutex::new(s),
+            cache_store: tokio::sync::Mutex::new(store::Store::new()),
+        }
+    }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     fix_path_env::fix().unwrap();
+
+    let db_url: &str = env!("DATABASE_URL");
+    let conn_options = SqliteConnectOptions::from_str(&db_url)
+        .unwrap()
+        .create_if_missing(true);
+    let db = SqlitePool::connect_with(conn_options).await.unwrap();
+    sqlx::migrate!().run(&db).await.unwrap();
 
     let (async_event_sender, mut async_event_receiver): (
         mpsc::Sender<events::AsyncEvent>,
         mpsc::Receiver<events::AsyncEvent>,
     ) = mpsc::channel(1);
-    let store = store::Store::new();
+
+    let app = App::new(db, async_event_sender);
 
     #[cfg(debug_assertions)]
     const LOG_TARGETS: [LogTarget; 2] = [LogTarget::Stdout, LogTarget::Webview];
@@ -49,13 +68,10 @@ fn main() {
     #[cfg(not(debug_assertions))]
     const LOG_TARGETS: [LogTarget; 2] = [LogTarget::Stdout, LogTarget::LogDir];
 
+    tauri::async_runtime::set(tokio::runtime::Handle::current());
+
     tauri::Builder::default()
-        .manage(AsyncEventSender {
-            inner: tokio::sync::Mutex::new(async_event_sender),
-        })
-        .manage(SadCache {
-            inner: tokio::sync::Mutex::new(store),
-        })
+        .manage(app)
         .plugin(
             tauri_plugin_log::Builder::default()
                 .targets(LOG_TARGETS)
@@ -69,6 +85,8 @@ fn main() {
             adb_disable_clear_and_stop_package,
             adb_enable_package,
             adb_install_package,
+            get_config,
+            update_config,
         ])
         .setup(|app| {
             let app_handle = app.handle();
@@ -80,13 +98,12 @@ fn main() {
                 }
             });
 
-            let app_handle2 = app.handle();
-
+            let app_handle = app.handle();
             tauri::async_runtime::spawn(async move {
                 let mut interval = time::interval(Duration::from_millis(3000));
                 loop {
                     interval.tick().await;
-                    track_devices(&app_handle2).await;
+                    track_devices(&app_handle).await;
                 }
             });
 
@@ -111,8 +128,8 @@ async fn track_devices<R: tauri::Runtime>(manager: &impl Manager<R>) {
         }
         Ok(device_with_users) => {
             let w = manager.get_window("main").unwrap();
-            let cache_state: tauri::State<'_, SadCache> = manager.state();
-            let mut cache = cache_state.inner.lock().await;
+            let app: tauri::State<'_, App> = manager.state();
+            let mut cache = app.cache_store.lock().await;
 
             for du in device_with_users {
                 let event = events::DeviceEvent::new(du.clone());
@@ -145,7 +162,7 @@ pub struct DeviceWithUsers {
 
 #[tauri::command]
 async fn adb_list_devices_with_users(
-    cache_state: tauri::State<'_, SadCache>,
+    app: tauri::State<'_, App>,
 ) -> Result<Vec<DeviceWithUsers>, SADError> {
     let res = _adb_list_device_with_users().await;
     match res {
@@ -153,7 +170,7 @@ async fn adb_list_devices_with_users(
             return Err(SADError::E(e));
         }
         Ok(device_with_users) => {
-            let mut cache = cache_state.inner.lock().await;
+            let mut cache = app.cache_store.lock().await;
 
             for du in device_with_users.clone() {
                 cache.insert_device_with_user(du.clone());
@@ -183,12 +200,12 @@ async fn _adb_list_device_with_users() -> anyhow::Result<Vec<DeviceWithUsers>> {
 async fn adb_list_packages(
     device_id: &str,
     user_id: &str,
-    cache_state: tauri::State<'_, SadCache>,
+    app: tauri::State<'_, App>,
 ) -> Result<Vec<Package>, SADError> {
     let acl = packages::ADBTerminalImpl {};
     let packages = acl.list_packages(device_id.to_string(), user_id.to_string())?;
 
-    let mut cache = cache_state.inner.lock().await;
+    let mut cache = app.cache_store.lock().await;
     let device = cache.device(device_id.to_owned())?;
     let user = device.user(user_id.to_owned())?;
     for p in packages.clone() {
@@ -202,14 +219,13 @@ async fn adb_disable_clear_and_stop_package(
     device_id: &str,
     user_id: &str,
     pkg: &str,
-    event_sender_state: tauri::State<'_, AsyncEventSender>,
-    cache_state: tauri::State<'_, SadCache>,
+    app: tauri::State<'_, App>,
 ) -> Result<(), SADError> {
     let acl = packages::ADBTerminalImpl {};
     acl.disable_package(device_id.to_string(), user_id.to_string(), pkg.to_string())?;
 
     {
-        let mut cache = cache_state.inner.lock().await;
+        let mut cache = app.cache_store.lock().await;
         let device = cache.device(device_id.to_owned())?;
         let user = device.user(user_id.to_owned())?;
         let package = user.get_package(pkg);
@@ -221,7 +237,7 @@ async fn adb_disable_clear_and_stop_package(
             Some(p) => {
                 p.set_state(packages::PackageState::Disabled);
                 let pe = PackageEvent::new(device_id.to_string(), user_id.to_string(), p.clone());
-                let esender = event_sender_state.inner.lock().await;
+                let esender = app.event_emitter.lock().await;
                 esender
                     .send(Box::new(pe))
                     .await
@@ -238,14 +254,13 @@ async fn adb_enable_package(
     device_id: &str,
     user_id: &str,
     pkg: &str,
-    event_sender_state: tauri::State<'_, AsyncEventSender>,
-    cache_state: tauri::State<'_, SadCache>,
+    app: tauri::State<'_, App>,
 ) -> Result<(), SADError> {
     let acl = packages::ADBTerminalImpl {};
     acl.enable_package(device_id.to_string(), user_id.to_string(), pkg.to_string())?;
 
     {
-        let mut cache = cache_state.inner.lock().await;
+        let mut cache = app.cache_store.lock().await;
         let device = cache.device(device_id.to_owned())?;
         let user = device.user(user_id.to_owned())?;
         let package = user.get_package(pkg);
@@ -257,7 +272,7 @@ async fn adb_enable_package(
             Some(p) => {
                 p.set_state(packages::PackageState::Enabled);
                 let pe = PackageEvent::new(device_id.to_string(), user_id.to_string(), p.clone());
-                let esender = event_sender_state.inner.lock().await;
+                let esender = app.event_emitter.lock().await;
                 esender
                     .send(Box::new(pe))
                     .await
@@ -274,14 +289,13 @@ async fn adb_install_package(
     device_id: &str,
     user_id: &str,
     pkg: &str,
-    event_sender_state: tauri::State<'_, AsyncEventSender>,
-    cache_state: tauri::State<'_, SadCache>,
+    app: tauri::State<'_, App>,
 ) -> Result<(), SADError> {
     let acl = packages::ADBTerminalImpl {};
     acl.install_package(device_id.to_string(), user_id.to_string(), pkg.to_string())?;
 
     {
-        let mut cache = cache_state.inner.lock().await;
+        let mut cache = app.cache_store.lock().await;
         let device = cache.device(device_id.to_owned())?;
         let user = device.user(user_id.to_owned())?;
         let package = user.get_package(pkg);
@@ -295,7 +309,7 @@ async fn adb_install_package(
             Some(p) => {
                 p.set_state(packages::PackageState::Enabled);
                 let pe = PackageEvent::new(device_id.to_string(), user_id.to_string(), p.clone());
-                let esender = event_sender_state.inner.lock().await;
+                let esender = app.event_emitter.lock().await;
                 esender
                     .send(Box::new(pe))
                     .await
@@ -305,4 +319,60 @@ async fn adb_install_package(
     }
 
     return Ok(());
+}
+
+#[tauri::command]
+async fn get_config(app: tauri::State<'_, App>) -> Result<String, SADError> {
+    let db = app.db.lock().await;
+
+    let default_id = 1;
+
+    let res = sqlx::query("SELECT * FROM config where id = ?")
+        .bind(default_id)
+        .fetch_one(&*db)
+        .await;
+
+    match res {
+        Err(e) => match e {
+            sqlx::Error::RowNotFound => {
+                return Err(anyhow!("config not found").into());
+            }
+            _ => {
+                return Err(anyhow!("error executing db: {}", e.to_string()).into());
+            }
+        },
+        Ok(r) => {
+            let res: Result<&str, sqlx::Error> = r.try_get("data");
+            match res {
+                Ok(d) => return Ok(d.to_string()),
+                Err(e) => {
+                    return Err(anyhow!("error getting data db: {}", e.to_string()).into());
+                }
+            }
+        }
+    }
+}
+
+#[tauri::command]
+async fn update_config(config: &str, app: tauri::State<'_, App>) -> Result<(), SADError> {
+    let db = app.db.lock().await;
+    let default_id = 1;
+    let res = sqlx::query(
+        "insert into config(id, data) VALUES($1, $2) ON CONFLICT(id) DO UPDATE SET data = $2",
+    )
+    .bind(default_id)
+    .bind(config)
+    .execute(&*db)
+    .await;
+    match res {
+        Ok(r) => {
+            if r.rows_affected() > 0 {
+                return Ok(());
+            }
+            return Err(anyhow!("no rows updated").into());
+        }
+        Err(e) => {
+            return Err(SADError::E(e.into()));
+        }
+    }
 }
