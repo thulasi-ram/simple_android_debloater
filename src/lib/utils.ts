c564955844e31fdc041c2ce3e7b6ce6bfb4400f1
Thulasi Ram <thulasi503@gmail.com>
2023-08-30 20:41:17 +0530

    fix: csv filter names

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,5 +4,5 @@ export const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
 export const isLastItem = (a: any[], i: number) => i == a.length - 1;
 
-export const JSON_DIALOG_FILTER: DialogFilter = { name: 'JSON', extensions: ['json'] };
-export const CSV_DIALOG_FILTER: DialogFilter = { name: 'JSON', extensions: ['json'] };
+export const JSON_DIALOG_FILTER: DialogFilter = { name: 'JSON File', extensions: ['json'] };
+export const CSV_DIALOG_FILTER: DialogFilter = { name: 'CSV File', extensions: ['csv', 'tsv'] };
